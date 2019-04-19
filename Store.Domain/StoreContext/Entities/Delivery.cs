26003083bd129379352a5ff4b80290cac954a12f
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-04-19 16:13:04 -0300

    Organize folders and update Entities

---
+++ Store.Domain/StoreContext/Entities/Delivery.cs
@@ -0,0 +1,19 @@
+using System;
+using Store.Domain.StoreContext.Enums;
+
+namespace Store.Domain.StoreContext.Entities
+{
+    public class Delivery
+    {
+        public Delivery(DateTime estimateDeliveryDate)
+        {
+            CreateDate = DateTime.Now;
+            EstimateDeliveryDate = estimateDeliveryDate;
+            Status = EDeliveryStatus.Waiting;
+        }
+
+        public DateTime CreateDate { get; private set; }
+        public DateTime EstimateDeliveryDate { get; private set; }
+        public EDeliveryStatus Status { get; private set; }
+    }
+}
