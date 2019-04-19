3201b2ac0e94a253a4e4f0167b93a01724295569
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-04-19 16:50:09 -0300

    Include Collections

--- Store.Domain/StoreContext/Entities/Customer.cs
+++ Store.Domain/StoreContext/Entities/Customer.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Store.Domain.StoreContext.ValueObjects;
 
 namespace Store.Domain.StoreContext.Entities
 {
     public class Customer
     {
+        private readonly IList<Address> _addresses;
         public Customer(
             Name name,
             Document document,
@@ -16,14 +18,22 @@ namespace Store.Domain.StoreContext.Entities
             Document = document;
             Email = email;
             Phone = phone;
-            Addresses = new List<Address>();
+            _addresses = new List<Address>();
         }
 
         public Name Name { get; private set; }
         public Document Document { get; private set; }
         public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public IReadOnlyCollection<Address> Addresses { get; private set; }
+        public IReadOnlyCollection<Address> Addresses => _addresses.ToArray();
+
+        public void AddAddress(Address address)
+        {
+            // Validate Address
+            
+            // Add Address
+            _addresses.Add(address);
+        }
 
         public override string ToString()
         {
