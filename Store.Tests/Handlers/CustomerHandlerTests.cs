310ccd88bd1f4536e51cb2df19e515a738ac7530
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-04-25 05:46:11 -0300

    Testing Handlers

--- Store.Tests/Handlers/CustomerHandlerTests.cs
+++ Store.Tests/Handlers/CustomerHandlerTests.cs
@@ -20,9 +20,11 @@ namespace Store.Tests.Handlers
             command.Email = "ricardocavalcantesilva@gmail.com";
             command.Phone = "5521999999999";
 
-            Assert.AreEqual(true, command.Valid());
-
             var handler = new CustomerHandler(new FakeCustomerRepository(), new FakeEmailService());
+            var result = handler.Handle(command);
+
+            Assert.AreNotEqual(null, result);
+            Assert.AreEqual(true, handler.IsValid);
         }
     }
 }
