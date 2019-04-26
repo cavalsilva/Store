68140ba17360ae1ba94787126798472a1ffb3392
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-04-26 05:44:46 -0300

    Create Data Context

---
+++ Store.Shared/Settings.cs
@@ -0,0 +1,7 @@
+namespace Store.Shared
+{
+    public static class Settings
+    {
+        public static string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Store;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
