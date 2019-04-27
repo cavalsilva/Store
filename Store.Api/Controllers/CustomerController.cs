37b4bdccf3a132f9bbefadbaee9dcbae3622d6f8
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-04-27 06:01:40 -0300

    Testing API CRUD

--- Store.Api/Controllers/CustomerController.cs
+++ Store.Api/Controllers/CustomerController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Store.Domain.StoreContext.CustomerCommands.Inputs;
 using Store.Domain.StoreContext.Entities;
+using Store.Domain.StoreContext.ValueObjects;
 
 namespace Store.Api.Controllers
 {
@@ -11,42 +13,79 @@ namespace Store.Api.Controllers
         [Route("customers")]
         public List<Customer> Get()
         {
-            return null;
+            var name = new Name("Ricardo", "Silva");
+            var document = new Document("13230609018");
+            var email = new Email("ricardocavalcantesilva@gmail.com");
+            var customer = new Customer(name, document, email, "5521999999999");
+            var customers = new List<Customer>();
+            customers.Add(customer);
+
+            return customers;
         }
 
         [HttpGet]
         [Route("customers/{id}")]
-        public List<Customer> GetById(Guid id)
+        public Customer GetById(Guid id)
         {
-            return null;
+            var name = new Name("Ricardo", "Silva");
+            var document = new Document("13230609018");
+            var email = new Email("ricardocavalcantesilva@gmail.com");
+            var customer = new Customer(name, document, email, "5521999999999");
+
+            return customer;
         }    
 
         [HttpGet]
         [Route("customers/{id}/orders")]
         public List<Order> GetOrders(Guid id)
         {
-            return null;
+            var name = new Name("Ricardo", "Silva");
+            var document = new Document("13230609018");
+            var email = new Email("ricardocavalcantesilva@gmail.com");
+            var customer = new Customer(name, document, email, "5521999999999");
+            var order = new Order(customer);
+            
+            var mouse = new Product("Mouse Gamer", "Mouse Gamer", "mouse.jpg", 100M, 10);
+            var monitor = new Product("Monitor Gamer", "Monitor Gamer", "monitor.jpg", 100M, 10);
+
+            order.AddItem(monitor, 5);
+            order.AddItem(mouse, 5);
+
+            var orders = new List<Order>();
+            orders.Add(order);
+
+            return orders;
         }
         
         [HttpPost]
         [Route("customers")]
-        public Customer Post([FromBody]Customer customer)
+        public Customer Post([FromBody]CreateCustomerCommand command)
         {
-            return null;
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document);
+            var email = new Email(command.Email);
+            var customer = new Customer(name, document, email, command.Phone);
+
+            return customer;
         }
 
         [HttpPut]
         [Route("customers/{id}")]
-        public Customer Put([FromBody]Customer customer)
+        public Customer Put([FromBody]CreateCustomerCommand command)
         {
-            return null;
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document);
+            var email = new Email(command.Email);
+            var customer = new Customer(name, document, email, command.Phone);
+
+            return customer;
         }
 
         [HttpDelete]
         [Route("customers/{id}")]
-        public string Delete(Guid id)
+        public object Delete(Guid id)
         {
-            return null;
+            return new { message = "Cliente removido com sucesso!" };
         }          
     }
 }
