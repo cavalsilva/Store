88013dbc3911419dab7df7523f45b3bdf2f21723
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-04-27 06:15:57 -0300

    DependencyInjection at ConfigureServices

--- Store.Api/Startup.cs
+++ Store.Api/Startup.cs
@@ -6,6 +6,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Store.Domain.StoreContext.Repositories;
+using Store.Domain.StoreContext.Services;
+using Store.Infra.Repositories;
+using Store.Infra.Services;
+using Store.Infra.StoreContext.DataContexts;
 
 namespace Store.Api
 {
@@ -16,6 +21,10 @@ namespace Store.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddScoped<StoreDataContext, StoreDataContext>();
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<IEmailService, EmailService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
