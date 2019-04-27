30acd63de7df3a495aa40f5c4edaa06eaaff9c32
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-04-27 05:36:12 -0300

    API CRUD

---
+++ Store.Api/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Store.Domain.StoreContext.Entities;
+
+namespace Store.Api.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        [Route("customers")]
+        public List<Customer> Get()
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("customers/{id}")]
+        public List<Customer> GetById(Guid id)
+        {
+            return null;
+        }    
+
+        [HttpGet]
+        [Route("customers/{id}/orders")]
+        public List<Order> GetOrders(Guid id)
+        {
+            return null;
+        }
+        
+        [HttpPost]
+        [Route("customers")]
+        public Customer Post([FromBody]Customer customer)
+        {
+            return null;
+        }
+
+        [HttpPut]
+        [Route("customers/{id}")]
+        public Customer Put([FromBody]Customer customer)
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        [Route("customers/{id}")]
+        public string Delete(Guid id)
+        {
+            return null;
+        }          
+    }
+}
