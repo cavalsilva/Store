399f6dd7d47bc9fcbf3490e469a46cf3954e39c4
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-04-28 05:39:33 -0300

    Using Dependency Injection API

--- Store.Api/Controllers/CustomerController.cs
+++ Store.Api/Controllers/CustomerController.cs
@@ -4,59 +4,41 @@ using Microsoft.AspNetCore.Mvc;
 using Store.Domain.StoreContext.CustomerCommands.Inputs;
 using Store.Domain.StoreContext.Entities;
 using Store.Domain.StoreContext.ValueObjects;
+using Store.Domain.StoreContext.Queries;
+using Store.Domain.StoreContext.Repositories;
 
 namespace Store.Api.Controllers
 {
     public class CustomerController : Controller
     {
+
+        private readonly ICustomerRepository _repository;
+        public CustomerController(ICustomerRepository repository)
+        {
+            _repository = repository;
+        }
+
         [HttpGet]
         [Route("customers")]
-        public List<Customer> Get()
+        public IEnumerable<ListCustomerQueryResult> Get()
         {
-            var name = new Name("Ricardo", "Silva");
-            var document = new Document("13230609018");
-            var email = new Email("ricardocavalcantesilva@gmail.com");
-            var customer = new Customer(name, document, email, "5521999999999");
-            var customers = new List<Customer>();
-            customers.Add(customer);
-
-            return customers;
+            return _repository.Get();
         }
 
         [HttpGet]
         [Route("customers/{id}")]
-        public Customer GetById(Guid id)
+        public GetCustomerQueryResult GetById(Guid id)
         {
-            var name = new Name("Ricardo", "Silva");
-            var document = new Document("13230609018");
-            var email = new Email("ricardocavalcantesilva@gmail.com");
-            var customer = new Customer(name, document, email, "5521999999999");
-
-            return customer;
-        }    
+            return _repository.Get(id);
+        }
 
         [HttpGet]
         [Route("customers/{id}/orders")]
-        public List<Order> GetOrders(Guid id)
+        public IEnumerable<ListCustomerOrdersQueryResult> GetOrders(Guid id)
         {
-            var name = new Name("Ricardo", "Silva");
-            var document = new Document("13230609018");
-            var email = new Email("ricardocavalcantesilva@gmail.com");
-            var customer = new Customer(name, document, email, "5521999999999");
-            var order = new Order(customer);
-            
-            var mouse = new Product("Mouse Gamer", "Mouse Gamer", "mouse.jpg", 100M, 10);
-            var monitor = new Product("Monitor Gamer", "Monitor Gamer", "monitor.jpg", 100M, 10);
-
-            order.AddItem(monitor, 5);
-            order.AddItem(mouse, 5);
-
-            var orders = new List<Order>();
-            orders.Add(order);
-
-            return orders;
+            return _repository.GetOrders(id);
         }
-        
+
         [HttpPost]
         [Route("customers")]
         public Customer Post([FromBody]CreateCustomerCommand command)
@@ -86,6 +68,6 @@ namespace Store.Api.Controllers
         public object Delete(Guid id)
         {
             return new { message = "Cliente removido com sucesso!" };
-        }          
+        }
     }
 }
